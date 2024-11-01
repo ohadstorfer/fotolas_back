3ed70f7430e000a11b50d81cd948a31f95f4d926
ohadstorfer <ohad.storfer@gmail.com>
2024-11-01 07:59:22 -0700

    the freshest123

--- myproj/settings.py
+++ myproj/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -224,5 +226,5 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-FRONTEND_URL = 'http://localhost:3000'  
+FRONTEND_URL = 'https://www.surfpik.com'  
 
