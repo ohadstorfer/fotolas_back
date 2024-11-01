f37850a2bf5a5e5480c5f22cd176194c46684a44
ohadstorfer <ohad.storfer@gmail.com>
2024-11-01 10:49:20 -0700

    the freshestforeal

--- myproj/settings.py
+++ myproj/settings.py
@@ -109,6 +109,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
