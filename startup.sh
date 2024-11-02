494a1e6bfd551914b059e9e96a6405907e3610ec
ohadstorfer <ohad.storfer@gmail.com>
2024-11-01 17:38:37 -0700

    the freshestforealnowww

--- startup.sh
+++ startup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 myproj.wsgi
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Run database migrations
+python manage.py migrate --noinput
+
+# Collect static files without prompting
+python manage.py collectstatic --noinput
+
+# Start the Gunicorn server
+exec gunicorn --workers 2 myproj.wsgi:application --bind 0.0.0.0:8080
