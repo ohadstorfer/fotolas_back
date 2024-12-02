4845ce8c2463bb725f02e44c6515e8403d66668c
ohadstorfer <ohad.storfer@gmail.com>
2024-12-02 08:58:09 -0600

    Your commit message

--- base/pagination.py
+++ base/pagination.py
@@ -1,6 +1,18 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+import math
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        total_pages = math.ceil(self.page.paginator.count / self.page_size)
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': total_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
